20bd95b105b6aceb0174be8078a2a585958d19ef
userc8e <caitlynquack@gmail.com>
2025-11-28 16:27:13 -0500

    added Comms, Enigma, Rotor files from Lab5

---
+++ Enigma.java
@@ -0,0 +1,82 @@
+public class Enigma{
+
+    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+        "#EJOTYCHMRWAFKPUZDINSXBGLQV",
+        "#BDFHJLNPRTVXZACEGIKMOQSUWY",
+        "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
+        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
+
+
+    private Rotor rotors[];
+    
+    
+    public Enigma(int id1, int id2, int id3, String start){
+
+        rotors = new Rotor[3];
+        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
+        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
+        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
+        
+    }
+
+
+    public String decrypt(String message){        
+        char[] msg = message.toCharArray(); //message in char array form
+        String answer = ""; //answer that will be returned at end of function
+
+        // goes through for loop to decrypt each char of message
+        for (int i = 0; i < msg.length; i++) {
+            char val = msg[i]; //the char being decrypted on this pass
+            int idx; //index on rotor
+
+            // goes through backwars rotor logic to find the decrypted value
+            idx = rotors[2].indexOf(val);
+            val = rotors[1].charAt(idx);
+            idx = rotors[2].indexOf(val);
+            val = rotors[0].charAt(idx);
+            
+
+            // adds result value to answer
+            answer += val;
+
+            // rotate rotors
+            if (rotors[0].rotate()) {
+                if (rotors[1].rotate()) {
+                    rotors[2].rotate();
+                }
+            }
+        }
+        return answer; //final decrypted message
+    }
+
+
+    
+    public String encrypt(String message){
+        char[] msg = message.toCharArray(); //message in char array form
+        String answer = ""; //answer that will be returned at the end of function
+
+        // goes through for loop to encrypt each char of message
+        for (int i = 0; i < msg.length; i++) {
+            char val = msg[i]; //the char being converted during this pass
+            int idx; //index on rotor
+
+            // goes through rotor logic to find the encrypted value
+            idx = rotors[0].indexOf(val);
+            val = rotors[2].charAt(idx);
+            idx = rotors[1].indexOf(val);
+            val = rotors[2].charAt(idx);
+
+            // adds this value to the answer
+            // (end of loop will result in a fully encrypted answer)
+            answer += val;
+
+            // rotate rotors
+            if (rotors[0].rotate()) {
+                if (rotors[1].rotate()) {
+                    rotors[2].rotate();
+                }
+            }
+        }
+        return answer; //final encrypted message
+    }
+}
---
+++ Rotor.java
@@ -0,0 +1,101 @@
+public class Rotor {
+
+    
+    private String rotorValues;
+    private char startChar;
+    private char[] rotorChars; //added - string form of rotorValues
+    private int count; //added - counts how many times rotor has been rotated
+        
+    public Rotor(String v, char c){
+        this.rotorValues = new String(v);
+        this.startChar = c;
+
+        rotorChars = rotorValues.toCharArray();
+
+        while (rotorChars[0] != startChar) {
+            this.rotate();
+        }
+          
+        count = 0;
+    }
+    
+    public boolean rotate(){
+        // creates a new array where the characters will be saved
+        char[] newArr = new char[rotorChars.length];
+
+        // sets first char in array to the last char in the old array
+        // as per the "circular" motion of rotating
+        newArr[0] = rotorChars[rotorChars.length-1];
+
+        // assigns every char a position following the newly added first char
+        for (int i = 1; i < rotorChars.length; i++) {
+            newArr[i] = rotorChars[i-1];
+        }
+
+        // sets new array values to the rotor
+        rotorChars = newArr;
+        rotorValues = new String(rotorChars);
+
+        count++; //adds every time the rotor rotates
+        // returns boolean for if rotor has made a full rotation
+        //return (rotorChars[0] == startChar);
+        if (count == rotorChars.length) {
+            count = 0; //resets
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // public boolean rotateBackwards() {
+    //     // creates a new array where the characters will be saved
+    //     char[] newArr = new char[rotorChars.length];
+
+    //     // sets last char in new array to the first char in old array
+    //     newArr[rotorChars.length-1] = rotorChars[0];
+
+    //     // assigns every char a position that is one step backwards
+    //     // does not include last index
+    //     for (int i = 0; i < rotorChars.length-1; i++) {
+    //         newArr[i] = rotorChars[i+1];
+    //     }
+
+    //     // sets new array values to the rotor
+    //     rotorChars = newArr;
+    //     rotorValues = new String(rotorChars);
+
+    //     count++; //adds every time the rotor rotates
+    //     // returns boolean for if rotor has made a full rotation
+    //     //return (rotorChars[0] == startChar);
+    //     if (count == rotorChars.length) {
+    //         count = 0; //resets
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    
+
+    public int indexOf(char c){
+        // goes through every char to find the first instance of c
+        // returns the index at which c is located
+        for (int i = 0; i < rotorValues.length(); i++) {
+            if (rotorChars[i] == (c)) {
+                return i;
+            }
+        }
+        // if c is not found in the string
+        return -1;
+    }
+
+    public char charAt(int idx){
+        // returns the char at the index
+        if (idx < rotorValues.length()) {
+            return rotorChars[idx];
+        }
+        // returns empty char if index is out of bounds
+        return ' ';
+    }
+}
+    
