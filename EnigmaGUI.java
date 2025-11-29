3349b823cd8cdaff6e0b13e09fbd51460953583d
userc8e <caitlynquack@gmail.com>
2025-11-28 23:32:44 -0500

    implemented action listeners for encrypt and decrypt

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -5,17 +5,40 @@ public class EnigmaGUI {
     public EnigmaGUI() {
         frame = new EnigmaFrame();
 
-        // ACTION LISTENERS
-
+        // ----------- ACTION LISTENERS --------------------------------
         // Encrypt Button
-        frame.bEncrypt.addActionListener(e) -> {
+        frame.bEncrypt.addActionListener((e) -> {
+            // rotor numbers
+            int inner = (Integer) frame.cbInner.getSelectedItem();
+            int middle = (Integer) frame.cbMiddle.getSelectedItem();
+            int out = (Integer) frame.cbOut.getSelectedItem();
+            String init = frame.tfInit.getText();
+            String input = frame.tfInput.getText();
 
-        }
+            // runs enigma encrypt method
+            enigma = new Enigma(inner, middle, out, init);
+            String encryption = enigma.encrypt(input);
+
+            // displays output
+            frame.tfOutput.setText(encryption);
+        });
 
         // Decrypt Button
-        frame.bDecrypt.addActionListener(e) -> {
+        frame.bDecrypt.addActionListener((e) -> {
+            // rotor numbers
+            int inner = (Integer) frame.cbInner.getSelectedItem();
+            int middle = (Integer) frame.cbMiddle.getSelectedItem();
+            int out = (Integer) frame.cbOut.getSelectedItem();
+            String init = frame.tfInit.getText();
+            String input = frame.tfInput.getText();
 
-        }
+            // runs enigma encrypt method
+            enigma = new Enigma(inner, middle, out, init);
+            String decryption = enigma.decrypt(input);
+
+            // displays output
+            frame.tfOutput.setText(decryption);
+        });
     }
 
 }
