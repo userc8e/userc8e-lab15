9479fe4c24ed4f79f5a44324d2279af28c99d99d
userc8e <caitlynquack@gmail.com>
2025-11-28 21:19:25 -0500

    skeleton for EnigmaGUI file

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,3 +1,21 @@
 public class EnigmaGUI {
-    
+    private EnigmaFrame frame;
+    private Enigma enigma;
+
+    public EnigmaGUI() {
+        frame = new EnigmaFrame();
+
+        // ACTION LISTENERS
+
+        // Encrypt Button
+        frame.bEncrypt.addActionListener(e) -> {
+
+        }
+
+        // Decrypt Button
+        frame.bDecrypt.addActionListener(e) -> {
+
+        }
+    }
+
 }
