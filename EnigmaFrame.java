ca0d7bf9feebc38991cc02cdbd230675d6bd4824
userc8e <caitlynquack@gmail.com>
2025-11-28 20:30:52 -0500

    added input and output panels

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -6,15 +6,13 @@ public class EnigmaFrame {
         f.setTitle("CSCI2113 GUI Lab15");
         f.setSize(1000, 600);
         f.setLocation(100, 100);
-        
-
-
-        Integer[] rotorNums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
-
+    
 
         // ----------- TOP PANEL --------------------------------
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new GridLayout(1, 6, 10, 0));
+
+        Integer[] rotorNums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
 
         // Inner Panel
         JPanel innerPanel = new JPanel();
@@ -82,7 +80,41 @@ public class EnigmaFrame {
         topPanel.add(decryptPanel);
         f.add(topPanel, BorderLayout.NORTH);    //add topPanel to top of frame
 
-            
+
+        // ----------- INPUT/OUTPUT PANEL --------------------------------
+        JPanel ioPanel = new JPanel();
+        ioPanel.setLayout(new GridLayout(2, 1, 10, 0));
+
+        // Input Panel
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BorderLayout());
+
+        JLabel lInput = new JLabel("Input ");
+        JTextField tfInput = new JTextField();
+        tfInput.setPreferredSize(new Dimension(100, 30));
+
+        inputPanel.add(lInput, BorderLayout.WEST);
+        inputPanel.add(tfInput, BorderLayout.CENTER);
+        //f.add(inputPanel, BorderLayout.CENTER);
+
+        // Output Panel
+        JPanel outputPanel = new JPanel();
+        outputPanel.setLayout(new BorderLayout());
+
+        JLabel lOutput = new JLabel("Output ");
+        JTextField tfOutput = new JTextField();
+        tfOutput.setPreferredSize(new Dimension(100, 30));
+
+        outputPanel.add(lOutput, BorderLayout.WEST);
+        outputPanel.add(tfOutput, BorderLayout.CENTER);
+        //f.add(outputPanel, BorderLayout.SOUTH);
+
+        // Put all together in Input/Output Panel
+        ioPanel.add(inputPanel);
+        ioPanel.add(outputPanel);
+        f.add(ioPanel, BorderLayout.CENTER);
+
+
         // ----------- END OF FRAME FILE --------------------------------
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
